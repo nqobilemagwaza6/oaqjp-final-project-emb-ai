7fb99e8b2ca3799d6db90bc860c2eaabf413a4a8
Nqobile <111858362+nqobilemagwaza6@users.noreply.github.com>
2026-03-18 10:03:38 +0200

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -5,19 +5,23 @@ app = Flask(__name__)
 
 @app.route('/emotionDetector', methods=['GET', 'POST'])
 def detect_emotion():
-    if request.method == 'POST':
-        statement = request.form.get('statement', '')
-        if statement:
-            result = emotion_detector(statement)
-            response_text = (f"For the given statement, the system response is "
-                             f"'anger': {result['anger']}, 'disgust': {result['disgust']}, "
-                             f"'fear': {result['fear']}, 'joy': {result['joy']} and "
-                             f"'sadness': {result['sadness']}. The dominant emotion is "
-                             f"{result['dominant_emotion']}.")
-            return render_template('index.html', result=response_text)
-        else:
-            return render_template('index.html', result="Please enter a statement.")
-    return render_template('index.html', result=None)
+    if request.method == "POST":
+        statement = request.form.get('statement', "")
+        result = emotion_detector(statement)
 
-if __name__ == '__main__':
+        if result['dominant_emotion'] is None:
+            response_text = "Invalid text! Please try again!"
+        else:
+            response_text = (
+                f"For the given statement, the system response is "
+                f"'anger': {result['anger']}, 'disgust': {result['disgust']}, "
+                f"'fear': {result['fear']}, 'joy': {result['joy']} and "
+                f"'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
+            )
+
+        return render_template('index.html', result=response_text)
+    else:
+        return render_template('index.html', result="")
+
+if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
